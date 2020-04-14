621eeadb62577a1b0e74a4c3a6c07484b29e8800
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-04-14 13:35:49 -0700

    adding new policy constants

--- policy/const.go
+++ policy/const.go
@@ -5,4 +5,25 @@ const (
 	plural   = "policies"
 )
 
+// Valid values for Policy.Rule.RuleType.
+const (
+	RuleTypeConfig     = "Config"
+	RuleTypeAuditEvent = "AuditEvent"
+	RuleTypeNetwork    = "Network"
+)
+
+// Valid values for Policy.PolicyType.
+const (
+	PolicyTypeConfig     = "config"
+	PolicyTypeAuditEvent = "audit_event"
+	PolicyTypeNetwork    = "network"
+)
+
+// Valid values for Policy.Rule.Severity.
+const (
+	SeverityLow    = "low"
+	SeverityMedium = "medium"
+	SeverityHigh   = "high"
+)
+
 var Suffix = []string{"policy"}
