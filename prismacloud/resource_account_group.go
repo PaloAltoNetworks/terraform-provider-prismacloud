c646f88c32f3133b98cdaa04520c7c4e598b9b50
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-04-28 12:37:00 -0700

    removing accounts and alert_rules from saved data until prisma cloud returns this info

--- prismacloud/resource_account_group.go
+++ prismacloud/resource_account_group.go
@@ -54,49 +54,51 @@ func resourceAccountGroup() *schema.Resource {
 					Type: schema.TypeString,
 				},
 			},
-			"accounts": {
-				Type:        schema.TypeList,
-				Computed:    true,
-				Description: "Associated cloud accounts",
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"account_id": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "Associated cloud account ID",
-						},
-						"name": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "Associated cloud account name",
-						},
-						"account_type": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "Associated cloud account type",
-						},
-					},
-				},
-			},
-			"alert_rules": {
-				Type:        schema.TypeList,
-				Computed:    true,
-				Description: "Singly associated alert rules which cannot exist in the system without the account group",
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"alert_id": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "The alert ID",
-						},
-						"name": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "Alert name",
+			/*
+				"accounts": {
+					Type:        schema.TypeList,
+					Computed:    true,
+					Description: "Associated cloud accounts",
+					Elem: &schema.Resource{
+						Schema: map[string]*schema.Schema{
+							"account_id": {
+								Type:        schema.TypeString,
+								Computed:    true,
+								Description: "Associated cloud account ID",
+							},
+							"name": {
+								Type:        schema.TypeString,
+								Computed:    true,
+								Description: "Associated cloud account name",
+							},
+							"account_type": {
+								Type:        schema.TypeString,
+								Computed:    true,
+								Description: "Associated cloud account type",
+							},
 						},
 					},
 				},
-			},
+				"alert_rules": {
+					Type:        schema.TypeList,
+					Computed:    true,
+					Description: "Singly associated alert rules which cannot exist in the system without the account group",
+					Elem: &schema.Resource{
+						Schema: map[string]*schema.Schema{
+							"alert_id": {
+								Type:        schema.TypeString,
+								Computed:    true,
+								Description: "The alert ID",
+							},
+							"name": {
+								Type:        schema.TypeString,
+								Computed:    true,
+								Description: "Alert name",
+							},
+						},
+					},
+				},
+			*/
 		},
 	}
 }
@@ -117,32 +119,36 @@ func saveAccountGroup(d *schema.ResourceData, obj group.Group) {
 	d.Set("description", obj.Description)
 	d.Set("last_modified_by", obj.LastModifiedBy)
 	d.Set("last_modified_ts", obj.LastModifiedTs)
-	if err = d.Set("account_ids", StringSliceToSet(obj.AccountIds)); err != nil {
+	if err = d.Set("account_ids", obj.AccountIds); err != nil {
 		log.Printf("[WARN] Error setting 'account_ids' field for %q: %s", d.Id(), err)
 	}
 
-	aInfo := make([]interface{}, 0, len(obj.Accounts))
-	for _, ai := range obj.Accounts {
-		aInfo = append(aInfo, map[string]interface{}{
-			"account_id":   ai.Id,
-			"name":         ai.Name,
-			"account_type": ai.Type,
-		})
-	}
-	if err = d.Set("accounts", aInfo); err != nil {
-		log.Printf("[WARN] Error setting 'accounts' field for %q: %s", d.Id(), err)
-	}
+	/*
+	   // Neither accounts nor alert rules is returned from the API when querying
+	   // a specific group ID (today).
+	   aInfo := make([]interface{}, 0, len(obj.Accounts))
+	   for _, ai := range obj.Accounts {
+	       aInfo = append(aInfo, map[string]interface{}{
+	           "account_id":   ai.Id,
+	           "name":         ai.Name,
+	           "account_type": ai.Type,
+	       })
+	   }
+	   if err = d.Set("accounts", aInfo); err != nil {
+	       log.Printf("[WARN] Error setting 'accounts' field for %q: %s", d.Id(), err)
+	   }
 
-	arInfo := make([]interface{}, 0, len(obj.AlertRules))
-	for _, ari := range obj.AlertRules {
-		arInfo = append(arInfo, map[string]interface{}{
-			"alert_id": ari.Id,
-			"name":     ari.Name,
-		})
-	}
-	if err = d.Set("alert_rules", arInfo); err != nil {
-		log.Printf("[WARN] Error setting 'alert_rules' field for %q: %s", d.Id(), err)
-	}
+	   arInfo := make([]interface{}, 0, len(obj.AlertRules))
+	   for _, ari := range obj.AlertRules {
+	       arInfo = append(arInfo, map[string]interface{}{
+	           "alert_id": ari.Id,
+	           "name":     ari.Name,
+	       })
+	   }
+	   if err = d.Set("alert_rules", arInfo); err != nil {
+	       log.Printf("[WARN] Error setting 'alert_rules' field for %q: %s", d.Id(), err)
+	   }
+	*/
 }
 
 func createAccountGroup(d *schema.ResourceData, meta interface{}) error {
