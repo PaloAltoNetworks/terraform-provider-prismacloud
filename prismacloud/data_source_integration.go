ad33a6f196e794425a6278a75238de7d06351421
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-05-11 09:45:37 -0700

    data source: integration details

---
+++ prismacloud/data_source_integration.go
@@ -0,0 +1,222 @@
+package prismacloud
+
+import (
+	pc "github.com/paloaltonetworks/prisma-cloud-go"
+	"github.com/paloaltonetworks/prisma-cloud-go/integration"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceIntegration() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceIntegrationRead,
+
+		Schema: map[string]*schema.Schema{
+			// Input.
+			"integration_id": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				Description:  "Integration ID",
+				AtLeastOneOf: []string{"integration_id", "name"},
+			},
+			"name": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				Description:  "Name of the integration",
+				AtLeastOneOf: []string{"integration_id", "name"},
+			},
+
+			// Output.
+			"description": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Description",
+			},
+			"integration_type": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Integration type",
+			},
+			"enabled": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Enabled",
+			},
+			"created_by": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Created by",
+			},
+			"created_ts": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Created on",
+			},
+			"last_modified_by": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Last modified by",
+			},
+			"last_modified_ts": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Last modified timestamp",
+			},
+			"status": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Status",
+			},
+			"valid": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Valid",
+			},
+			"reason": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "Model for the integration status details",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"last_updated": {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Last updated",
+						},
+						"error_type": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Error type",
+						},
+						"message": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Message",
+						},
+						"details": {
+							Type:        schema.TypeList,
+							Computed:    true,
+							Description: "Model for message details",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"status_code": {
+										Type:        schema.TypeInt,
+										Computed:    true,
+										Description: "Status code",
+									},
+									"subject": {
+										Type:        schema.TypeString,
+										Computed:    true,
+										Description: "Subject",
+									},
+									"message": {
+										Type:        schema.TypeString,
+										Computed:    true,
+										Description: "Internationalization key",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			"integration_config": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "Integration configuration, the values depend on the integration type",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"queue_url": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The Queue URL you used when you configured Prisma Cloud in Amazon SQS",
+						},
+						"login": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "(Qualys/ServiceNow) Login",
+						},
+						"base_url": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Qualys Security Operations Center server API URL (without \"http(s)\")",
+						},
+						"password": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "(Qualys/ServiceNow) Password",
+						},
+						"host_url": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "ServiceNow URL",
+						},
+						"tables": {
+							Type:        schema.TypeMap,
+							Computed:    true,
+							Description: "Key/value pairs that identify the ServiceNow module tables with which to integrate (e.g. - incident, sn_si_incident, or em_event)",
+							Elem: &schema.Schema{
+								Type: schema.TypeBool,
+							},
+						},
+						"version": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "ServiceNow release version",
+						},
+						"url": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Webhook URL",
+						},
+						"auth_token": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "(Webhook/PagerDuty) The authentication token for the event collector",
+						},
+						"integration_key": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "PagerDuty integration key",
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceIntegrationRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*pc.Client)
+
+	var err error
+	id := d.Get("integration_id").(string)
+
+	if id == "" {
+		name := d.Get("name").(string)
+		id, err = integration.Identify(client, name)
+		if err != nil {
+			if err == pc.ObjectNotFoundError {
+				d.SetId("")
+				return nil
+			}
+			return err
+		}
+	}
+
+	o, err := integration.Get(client, id)
+	if err != nil {
+		if err == pc.ObjectNotFoundError {
+			d.SetId("")
+			return nil
+		}
+		return err
+	}
+
+	d.SetId(o.Id)
+	saveIntegration(d, o)
+
+	return nil
+}
