c4b2bf2e1042f1a85f7a933d4a13346c407dafcb
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-06-02 16:15:20 -0700

    test: acctest for alert rules data source

---
+++ prismacloud/data_source_alert_rules_test.go
@@ -0,0 +1,28 @@
+package prismacloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccDsAlertRules(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDsAlertRulesConfig(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.prismacloud_alert_rules.test", "total"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDsAlertRulesConfig() string {
+	return `
+data "prismacloud_alert_rules" "test" {}
+`
+}
