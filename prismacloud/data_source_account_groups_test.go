111dead886bd31b077c5e4a7744d7e34a4e4c787
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-06-02 16:04:53 -0700

    test: adding data source account groups acctest

---
+++ prismacloud/data_source_account_groups_test.go
@@ -0,0 +1,28 @@
+package prismacloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccDsAccountGroups(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDsAccountGroupsConfig(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.prismacloud_account_groups.test", "total"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDsAccountGroupsConfig() string {
+	return `
+data "prismacloud_account_groups" "test" {}
+`
+}
