ed8e38350479cf81c9c881128c9b71821c218b56
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-06-05 13:04:47 -0700

    +acctest for csrs listing data source

---
+++ prismacloud/data_source_compliance_standard_requirement_section_test.go
@@ -0,0 +1,55 @@
+package prismacloud
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccDsComplianceStandardRequirementSection(t *testing.T) {
+	cs := fmt.Sprintf("tf%s", acctest.RandString(6))
+	csr := fmt.Sprintf("tf%s", acctest.RandString(6))
+	csrs := fmt.Sprintf("tf%s", acctest.RandString(6))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDsComplianceStandardRequirementSectionConfig(cs, csr, csrs),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.prismacloud_compliance_standard_requirement_section.test", "csrs_id"),
+					resource.TestCheckResourceAttrSet("data.prismacloud_compliance_standard_requirement_section.test", "section_id"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDsComplianceStandardRequirementSectionConfig(cs, csr, csrs string) string {
+	return fmt.Sprintf(`
+data "prismacloud_compliance_standard_requirement_section" "test" {
+    csr_id = prismacloud_compliance_standard_requirement_section.x.csr_id
+    csrs_id = prismacloud_compliance_standard_requirement_section.x.csrs_id
+}
+
+resource "prismacloud_compliance_standard" "x" {
+    name = %q
+    description = "csrs data source acctest"
+}
+
+resource "prismacloud_compliance_standard_requirement" "x" {
+    cs_id = prismacloud_compliance_standard.x.cs_id
+    name = %q
+    description = "csrs data source acctest"
+}
+
+resource "prismacloud_compliance_standard_requirement_section" "x" {
+    csr_id = prismacloud_compliance_standard_requirement.x.csr_id
+    section_id = %q
+    description = "csrs data source acctest"
+}
+`, cs, csr, csrs)
+}
