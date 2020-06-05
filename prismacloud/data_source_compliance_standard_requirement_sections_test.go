c55faedf83dc57d0a92e71d72303ba8135b30055
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-06-05 13:05:11 -0700

    +acctest for csrs listing data source

---
+++ prismacloud/data_source_compliance_standard_requirement_sections_test.go
@@ -0,0 +1,53 @@
+package prismacloud
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccDsComplianceStandardRequirementSections(t *testing.T) {
+	cs := fmt.Sprintf("tf%s", acctest.RandString(6))
+	csr := fmt.Sprintf("tf%s", acctest.RandString(6))
+	csrs := fmt.Sprintf("tf%s", acctest.RandString(6))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDsComplianceStandardRequirementSectionsConfig(cs, csr, csrs),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.prismacloud_compliance_standard_requirement_sections.test", "total"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDsComplianceStandardRequirementSectionsConfig(cs, csr, csrs string) string {
+	return fmt.Sprintf(`
+data "prismacloud_compliance_standard_requirement_sections" "test" {
+    csr_id = prismacloud_compliance_standard_requirement.x.csr_id
+}
+
+resource "prismacloud_compliance_standard" "x" {
+    name = %q
+    description = "csrs listing data source acctest"
+}
+
+resource "prismacloud_compliance_standard_requirement" "x" {
+    cs_id = prismacloud_compliance_standard.x.cs_id
+    name = %q
+    description = "csrs listing data source acctest"
+}
+
+resource "prismacloud_compliance_standard_requirement_section" "x" {
+    csr_id = prismacloud_compliance_standard_requirement.x.csr_id
+    section_id = %q
+    description = "csrs listing data source acctest"
+}
+`, cs, csr, csrs)
+}
