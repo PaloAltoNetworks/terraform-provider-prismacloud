665172ca7bb58b96fe7e51ff2e5f0e362b6560c8
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-06-23 19:44:16 -0700

    adding historic rql searches ds

---
+++ prismacloud/data_source_rql_historic_searches.go
@@ -0,0 +1,117 @@
+package prismacloud
+
+import (
+	"log"
+	"strconv"
+
+	pc "github.com/paloaltonetworks/prisma-cloud-go"
+	"github.com/paloaltonetworks/prisma-cloud-go/rql/history"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceRqlHistoricSearches() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceRqlHistoricSearchesRead,
+
+		Schema: map[string]*schema.Schema{
+			// Input.
+			"filter": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Filter for historic RQL searches",
+				Default:     history.Saved,
+				ValidateFunc: validation.StringInSlice(
+					[]string{
+						history.Recent,
+						history.Saved,
+					},
+					false,
+				),
+			},
+			"limit": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Description: "Max number of historic RQL searches to return",
+				Default:     1000,
+			},
+
+			// Output.
+			"total": totalSchema("RQL saved searches"),
+			"listing": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "List of historic RQL searches",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"created_by": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Created by",
+						},
+						"last_modified_by": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Last modified by",
+						},
+						"search_id": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Historic RQL search ID",
+						},
+						"name": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Name",
+						},
+						"search_type": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Search type",
+						},
+						"saved": {
+							Type:        schema.TypeBool,
+							Computed:    true,
+							Description: "If this is a saved search",
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceRqlHistoricSearchesRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*pc.Client)
+
+	filter := d.Get("filter").(string)
+	limit := d.Get("limit").(int)
+
+	items, err := history.List(client, filter, limit)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(TwoStringsToId(filter, strconv.Itoa(limit)))
+
+	d.Set("total", len(items))
+
+	list := make([]interface{}, 0, len(items))
+	for _, i := range items {
+		list = append(list, map[string]interface{}{
+			"created_by":       i.CreatedBy,
+			"last_modified_by": i.LastModifiedBy,
+			"search_id":        i.Model.Id,
+			"name":             i.Model.Name,
+			"search_type":      i.Model.SearchType,
+			"saved":            i.Model.Saved,
+		})
+	}
+
+	if err := d.Set("listing", list); err != nil {
+		log.Printf("[WARN] Error setting 'listing' field for %q: %s", d.Id(), err)
+	}
+
+	return nil
+}
