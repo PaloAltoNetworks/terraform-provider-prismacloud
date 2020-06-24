fbdb7ea4e51ec03e6ee9104e0ae779d5686c354e
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-06-23 19:43:47 -0700

    adding rql historic search ds

---
+++ prismacloud/data_source_rql_historic_search.go
@@ -0,0 +1,104 @@
+package prismacloud
+
+import (
+	"log"
+
+	pc "github.com/paloaltonetworks/prisma-cloud-go"
+	"github.com/paloaltonetworks/prisma-cloud-go/rql/history"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceRqlHistoricSearch() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceRqlHistoricSearchRead,
+
+		Schema: map[string]*schema.Schema{
+			// Input.
+			"search_id": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				Description:  "Historic RQL search ID",
+				AtLeastOneOf: []string{"name", "search_id"},
+			},
+			"name": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				Description:  "Historic RQL search name",
+				AtLeastOneOf: []string{"name", "search_id"},
+			},
+
+			// Output.
+			"description": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Description",
+			},
+			"search_type": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Search type",
+			},
+			"cloud_type": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Cloud type",
+			},
+			"query": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "RQL query",
+			},
+			"saved": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "If this is a saved search",
+			},
+			"time_range": timeRangeSchema("data_source_rql_historic_search"),
+		},
+	}
+}
+
+func dataSourceRqlHistoricSearchRead(d *schema.ResourceData, meta interface{}) error {
+	var err error
+	client := meta.(*pc.Client)
+
+	id := d.Get("search_id").(string)
+	if id == "" {
+		name := d.Get("name").(string)
+		id, err = history.Identify(client, name)
+		if err != nil {
+			if err == pc.ObjectNotFoundError {
+				d.SetId("")
+				return nil
+			}
+			return err
+		}
+	}
+
+	o, err := history.Get(client, id)
+	log.Printf("Got: %#v", o)
+	if err != nil {
+		if err == pc.ObjectNotFoundError {
+			d.SetId("")
+			return nil
+		}
+		return err
+	}
+
+	d.SetId(id)
+	d.Set("search_id", o.Id)
+	d.Set("name", o.Name)
+	d.Set("description", o.Description)
+	d.Set("search_type", o.SearchType)
+	d.Set("cloud_type", o.CloudType)
+	d.Set("query", o.Query)
+	d.Set("saved", o.Saved)
+	if err = d.Set("time_range", flattenTimeRange(o.TimeRange)); err != nil {
+		log.Printf("[WARN] Error setting 'time_range' for %q: %s", d.Id(), err)
+	}
+
+	return nil
+}
