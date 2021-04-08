11abbacab61105fec9d84f583d3b696ff480bdba
erlend-vimond <73222545+erlend-vimond@users.noreply.github.com>
2021-04-08 22:37:19 +0200

    Fixed incorrect map key when parsing alert-rule target tags values (#22)
    * Fixed incorrect map key when parsing alert-rule target tags values
    
    * Added a test for alert rule tags

--- prismacloud/resource_alert_rule_test.go
+++ prismacloud/resource_alert_rule_test.go
@@ -17,6 +17,17 @@ func TestAccAlertRule(t *testing.T) {
 	grp := fmt.Sprintf("tf%s", acctest.RandString(6))
 	name := fmt.Sprintf("tf%s", acctest.RandString(6))
 
+	tags := []rule.Tag{
+		{
+			Key:    fmt.Sprintf("tf%s", acctest.RandString(6)),
+			Values: []string{fmt.Sprintf("tf%s", acctest.RandString(6)), fmt.Sprintf("tf%s", acctest.RandString(6))},
+		},
+		{
+			Key:    fmt.Sprintf("tf%s", acctest.RandString(6)),
+			Values: []string{fmt.Sprintf("tf%s", acctest.RandString(6)), fmt.Sprintf("tf%s", acctest.RandString(6))},
+		},
+	}
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
@@ -26,14 +37,21 @@ func TestAccAlertRule(t *testing.T) {
 				Config: testAccAlertRuleConfig(grp, name, "alert rule desc1"),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAlertRuleExists("prismacloud_alert_rule.test", &o),
-					testAccCheckAlertRuleAttributes(&o, name, "alert rule desc1"),
+					testAccCheckAlertRuleAttributes(&o, name, "alert rule desc1", nil),
 				),
 			},
 			{
 				Config: testAccAlertRuleConfig(grp, name, "alert rule desc2"),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAlertRuleExists("prismacloud_alert_rule.test", &o),
-					testAccCheckAlertRuleAttributes(&o, name, "alert rule desc2"),
+					testAccCheckAlertRuleAttributes(&o, name, "alert rule desc2", nil),
+				),
+			},
+			{
+				Config: testAccAlertRuleConfigTags(grp, name, "alert rule tags", tags),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAlertRuleExists("prismacloud_alert_rule.test", &o),
+					testAccCheckAlertRuleAttributes(&o, name, "alert rule with tags", tags),
 				),
 			},
 		},
@@ -64,7 +82,7 @@ func testAccCheckAlertRuleExists(n string, o *rule.Rule) resource.TestCheckFunc 
 	}
 }
 
-func testAccCheckAlertRuleAttributes(o *rule.Rule, name, desc string) resource.TestCheckFunc {
+func testAccCheckAlertRuleAttributes(o *rule.Rule, name, desc string, tags []rule.Tag) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		if o.Name != name {
 			return fmt.Errorf("Name is %s, expected %s", o.Name, name)
@@ -72,6 +90,21 @@ func testAccCheckAlertRuleAttributes(o *rule.Rule, name, desc string) resource.T
 
 		if o.Description != desc {
 			return fmt.Errorf("Description is %s, expected %s", o.Description, desc)
+		}
+
+		if tags != nil {
+			for i := 0; i < len(tags); i++ {
+				if o.Target.Tags[i].Key != tags[i].Key {
+					return fmt.Errorf("Tags is %+v, expected %+v", o.Target.Tags, tags)
+				}
+
+				for j := 0; j < len(tags[i].Values); j++ {
+					if o.Target.Tags[i].Values[j] != tags[i].Values[j] {
+						return fmt.Errorf("Tags is %+v, expected %+v", o.Target.Tags, tags)
+					}
+				}
+			}
+
 		}
 
 		return nil
@@ -117,4 +150,34 @@ resource "prismacloud_alert_rule" "test" {
     }
 }
 `, grp, name, desc)
+}
+
+func testAccAlertRuleConfigTags(grp, name, desc string, tags []rule.Tag) string {
+	return fmt.Sprintf(`
+	resource "prismacloud_account_group" "x" {
+		name = %q
+		description = "for alert rule acctest"
+	}
+	
+	resource "prismacloud_alert_rule" "test" {
+		name = %q
+		description = %q
+		enabled = false
+		scan_all = true
+		target {
+			account_groups = [
+				prismacloud_account_group.x.group_id,
+			]
+			tags {
+				key = "%s"
+				values = ["%s", "%s"]
+			}
+
+			tags {
+				key = "%s"
+				values = ["%s", "%s"]
+			}
+		}
+	}
+	`, grp, name, desc, tags[0].Key, tags[0].Values[0], tags[0].Values[1], tags[1].Key, tags[1].Values[0], tags[1].Values[1])
 }
