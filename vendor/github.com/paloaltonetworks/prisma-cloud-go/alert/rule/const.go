bf370b4ef642b81bb5b24ba241b857b6ea864b4b
Eddie Beuerlein <eddie@bluemojofarm.com>
2021-10-29 15:53:17 +0000

    Updating vendor libs

--- vendor/github.com/paloaltonetworks/prisma-cloud-go/alert/rule/const.go
+++ vendor/github.com/paloaltonetworks/prisma-cloud-go/alert/rule/const.go
@@ -16,7 +16,6 @@ const (
 	TypeSlack                = "slack"
 	TypeSplunk               = "splunk"
 	TypeAmazonSqs            = "amazon_sqs"
-	TypeJira                 = "jira"
 	TypeMicrosoftTeams       = "microsoft_teams"
 	TypeWebhook              = "webhook"
 	TypeAwsSecurityHub       = "aws_security_hub"
@@ -25,6 +24,8 @@ const (
 	TypePagerDuty            = "pager_duty"
 	TypeDemisto              = "demisto"
 	TypeAzureServiceBusQueue = "azure_service_bus_queue"
+	TypeSnowFlake            = "snowflake"
+	TypeAwsS3                = "aws_s3"
 )
 
 const (
