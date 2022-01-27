86dadd87c0439dfceeeda105e58b07efe8cc0769
Suranya Sunkavilli <80334497+sunkavillisuranya@users.noreply.github.com>
2022-01-27 22:18:25 +0530

    added test files for user resource (#106)

---
+++ prismacloud/data_source_user_role_test.go
@@ -0,0 +1,41 @@
+package prismacloud
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccDsUserRole(t *testing.T) {
+	name := fmt.Sprintf("tf%s", acctest.RandString(6))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDsUserRole(name),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.prismacloud_user_role.test", "terraform_user"),
+					resource.TestCheckResourceAttrSet("data.prismacloud_user_role.test", "name"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDsUserRole(name string) string {
+	return fmt.Sprintf(`
+resource "prismacloud_user_role" "x" {
+    name = %q
+    description = "User Role for Terraform"
+	role_type = "System Admin"
+}
+
+data "prismacloud_user_role" "test" {
+    role_id = prismacloud_user_role.x.role_id
+}
+`, name)
+}
---
+++ prismacloud/data_source_user_roles_test.go
@@ -0,0 +1,28 @@
+package prismacloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccDsUserRoles(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDsUserRoles(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.prismacloud_user_roles.test", "total"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDsUserRoles() string {
+	return `
+data "prismacloud_user_roles" "test" {}
+`
+}
