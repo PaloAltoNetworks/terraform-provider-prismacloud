0b57133a77386c6ffa8f0d749015266275c39448
Trishala Jadhav <88381436+trishala1999@users.noreply.github.com>
2022-04-26 15:22:33 +0530

    Adding support for AccessKeyMaxValidity and AlarmEnabled in enterprise settings schema (#26)

--- settings/enterprise/const.go
+++ settings/enterprise/const.go
@@ -1,12 +1,5 @@
 package enterprise
 
-// Valid values for AnomalyTrainingModelThreshold and AnomalyAlertDisposition.
-const (
-	Low    = "low"
-	Medium = "medium"
-	High   = "high"
-)
-
 const (
 	singular = "enterprise settings"
 )
