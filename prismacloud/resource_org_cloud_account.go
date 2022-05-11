b21c16a8f69f43b6cfe832d6afa0bfdbcf43a83d
nikitapanw <naware@paloaltonetworks.com>
2022-05-11 12:23:12 +0530

    Azure hierarchy

--- prismacloud/resource_org_cloud_account.go
+++ prismacloud/resource_org_cloud_account.go
@@ -257,6 +257,56 @@ func resourceOrgCloudAccount() *schema.Resource {
 							Description: "Application ID key",
 							Sensitive:   true,
 						},
+						"root_sync_enabled": {
+							Type:        schema.TypeBool,
+							Required:    true,
+							Description: "true = Azure tenant has children. Must be set to true",
+						},
+						"hierarchy_selection": {
+							Type:        schema.TypeSet,
+							Optional:    true,
+							Description: "List of subscriptions and/or management groups to onboard",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"resource_id": {
+										Type:        schema.TypeString,
+										Required:    true,
+										Description: "Resource ID. Management group ID or subscription ID.\nNote you must escape any double quotes in the resource ID with a backslash.",
+									},
+									"display_name": {
+										Type:        schema.TypeString,
+										Required:    true,
+										Description: "Display name for management group or subscription",
+									},
+									"node_type": {
+										Type:        schema.TypeString,
+										Required:    true,
+										Description: "Node type. Valid values: SUBSCRIPTION, TENANT, MANAGEMENT_GROUP",
+										ValidateFunc: validation.StringInSlice(
+											[]string{
+												"SUBSCRIPTION",
+												"TENANT",
+												"MANAGEMENT_GROUP",
+											},
+											false,
+										),
+									},
+									"selection_type": {
+										Type:        schema.TypeString,
+										Required:    true,
+										Description: "Selection type. Valid values: INCLUDE to include the specified resource to onboard, EXCLUDE to exclude the specified resource and onboard the rest, ALL to onboard all resources in the tenant.",
+										ValidateFunc: validation.StringInSlice(
+											[]string{
+												"INCLUDE",
+												"EXCLUDE",
+												"ALL",
+											},
+											false,
+										),
+									},
+								},
+							},
+						},
 					},
 				},
 			},
