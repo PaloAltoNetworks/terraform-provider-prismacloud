b7e830c3bfef8aa8be8615f72eb6ab265b45d64c
trishala1999 <tanupjadhav@paloaltonetworks.com>
2022-12-23 15:38:13 +0530

    Updating vendor dependencies

--- vendor/github.com/paloaltonetworks/prisma-cloud-go/policy/const.go
+++ vendor/github.com/paloaltonetworks/prisma-cloud-go/policy/const.go
@@ -7,12 +7,13 @@ const (
 
 // Valid values for Policy.Rule.RuleType.
 const (
-	RuleTypeConfig     = "Config"
-	RuleTypeAuditEvent = "AuditEvent"
-	RuleTypeNetwork    = "Network"
-	RuleTypeIAM        = "IAM"
-	RuleTypeAnomaly    = "Anomaly"
-	RuleTypeData       = "DLP"
+	RuleTypeConfig        = "Config"
+	RuleTypeAuditEvent    = "AuditEvent"
+	RuleTypeNetwork       = "Network"
+	RuleTypeIAM           = "IAM"
+	RuleTypeAnomaly       = "Anomaly"
+	RuleTypeData          = "DLP"
+	RuleTypeNetworkConfig = "NetworkConfig"
 )
 
 // Valid values for Policy.PolicyType.
@@ -27,9 +28,11 @@ const (
 
 // Valid values for Policy.Rule.Severity.
 const (
-	SeverityLow    = "low"
-	SeverityMedium = "medium"
-	SeverityHigh   = "high"
+	SeverityLow           = "low"
+	SeverityMedium        = "medium"
+	SeverityHigh          = "high"
+	SeverityCritical      = "critical"
+	SeverityInformational = "informational"
 )
 
 var Suffix = []string{"policy"}
